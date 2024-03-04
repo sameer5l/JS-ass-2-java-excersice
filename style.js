3d3db5f311a624e27a9ae2821c210ca690b2fe07
sameer5l <info.sameer25@gmail.com>
2024-03-05 00:09:38 +0500

    Initial commit

---
+++ style.js
@@ -0,0 +1,24 @@
+// VARIABLE NAMES: LEGAL & ILLEGAL
+
+// Question 1
+// var name; var age; var year;
+
+// Question 2
+// legal variable names.
+// useralert
+// heading1
+// fullName
+// full_name
+// userdocument
+
+// illegal variable names.
+// alert
+// 1number
+// document
+// fullnamee
+// full-name
+
+
+// // Question 3
+// document.write("<h1>“Rules for naming JS variables”</h1> ")
+// document.write("<p>Variable names can only contain . numbers , $ and _. For example : $my_1stVariable  <br> Varialbe must beginwith a letter, $ or _ , For example: $name or name  <br> Variable names are case sensitive <br> Variable names should not be JS keyword  </p>")
