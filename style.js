77431bca3767941640d2b53bd68e9ebe0c0f817a
Sameer Shahzad <155289046+sameer5l@users.noreply.github.com>
2024-03-08 23:34:03 +0500

    Update style.js

--- style.js
+++ style.js
@@ -22,3 +22,54 @@
 // // Question 3
 // document.write("<h1>“Rules for naming JS variables”</h1> ")
 // document.write("<p>Variable names can only contain . numbers , $ and _. For example : $my_1stVariable  <br> Varialbe must beginwith a letter, $ or _ , For example: $name or name  <br> Variable names are case sensitive <br> Variable names should not be JS keyword  </p>")
+
+
+// MATH EXPRESSIONS
+
+// Question 1
+// var num3=3
+// var num5=5
+// var numsum=num3+num5
+// var sume="Sum of 3 and 5 is "
+// document.write(sume+numsum)
+
+// Question 2
+// Subtraction
+
+// var num3=3
+// var num5=5
+// var numsum=num3-num5
+// var sume="Subtraction of 3 and 5 is "
+// document.write(sume+numsum)
+
+// Division
+
+// var num3=3
+// var num5=5
+// var numsum=num3/num5
+// var sume="Division of 3 and 5 is "
+// document.write(sume+numsum)
+
+// Multiplication
+
+// var num3=3
+// var num5=5
+// var numsum=num3*num5
+// var sume="Multiplication of 3 and 5 is "
+// document.write(sume+numsum)
+
+// Modulus
+
+// var num3=3
+// var num5=5
+// var numsum=num3%num5
+// var sume="Modulus of 3 and 5 is "
+// document.write(sume+numsum)
+
+
+// // Quesiton 4
+// var ticketprice=600
+// var ticket="The cost to buy 5 tickets to a movie is "
+// var pkr=" PKR"
+// var pricee=ticketprice*5
+// document.write(ticket+pricee+pkr)
